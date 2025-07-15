29d45f11b01c0dc82e311bb2874f5a33d0934206
yusay1498 <u.yusay1498@gmail.com>
2025-07-15 16:41:39 +0900

    add ArithmeticException demo by div

--- java-basics/demo-exception/src/main/java/com/example/Main.java
+++ java-basics/demo-exception/src/main/java/com/example/Main.java
@@ -1,17 +1,18 @@
 package com.example;
 
-//TIP コードを<b>実行</b>するには、<shortcut actionId="Run"/> を押すか
-// ガターの <icon src="AllIcons.Actions.Execute"/> アイコンをクリックします。
 public class Main {
     public static void main(String[] args) {
-        //TIP ハイライトされたテキストにキャレットがある状態で <shortcut actionId="ShowIntentionActions"/> を押すと
-        // IntelliJ IDEA によるその修正案を確認できます。
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP <shortcut actionId="Debug"/> を押してコードのデバッグを開始します。<icon src="AllIcons.Debugger.Db_set_breakpoint"/> ブレークポイントを 1 つ設定しましたが、
-            // <shortcut actionId="ToggleLineBreakpoint"/> を押すといつでも他のブレークポイントを追加できます。
-            System.out.println("i = " + i);
+        Calculator calculator = new Calculator();
+
+        int a = 10;
+        int b = 0;
+
+        try {
+            int answer = calculator.div(a, b);
+            System.out.println(answer);
+        }  catch (ArithmeticException e) {
+            System.err.println(e.getMessage());
         }
     }
 }
