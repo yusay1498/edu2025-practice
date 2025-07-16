44fdeb20b930a23ea2c700743d10f231873110a1
yusay1498 <u.yusay1498@gmail.com>
2025-07-16 14:55:30 +0900

    check absolute path

--- java-basics/demo-file/src/main/java/com/example/Main.java
+++ java-basics/demo-file/src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+import java.nio.file.Path;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Path path = Path.of("/home/ubuntu/hoge/fuga.txt");
+
+        System.out.println(path);
+        System.out.println(path.getRoot());
+        System.out.println(path.getParent());
+        System.out.println(path.getFileName());
     }
 }
