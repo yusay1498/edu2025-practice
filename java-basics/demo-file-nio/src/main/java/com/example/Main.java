a39aa119e56ad455b92c45a8c5039649de5622c2
yusay1498 <u.yusay1498@gmail.com>
2025-07-17 15:03:29 +0900

    check PrintWriter method

--- java-basics/demo-file-nio/src/main/java/com/example/Main.java
+++ java-basics/demo-file-nio/src/main/java/com/example/Main.java
@@ -2,7 +2,9 @@ package com.example;
 
 import javax.imageio.IIOException;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -16,6 +18,17 @@ public class Main {
                 System.out.println(line);
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (PrintWriter out = new PrintWriter("./demo-file-nio/src/main/resources/data.txt")){
+            out.print(100);
+            out.print("\t");
+            out.print("田中");
+            out.print("\t");
+            out.print(60.5);
+            out.print(100);
+        } catch(IOException e) {
             e.printStackTrace();
         }
     }
