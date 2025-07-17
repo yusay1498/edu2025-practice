994caa0dc5a5531ba60a74a2d4d4456cdfac5ec1
yusay1498 <u.yusay1498@gmail.com>
2025-07-17 11:16:08 +0900

    check toAbsolutePath

--- java-basics/demo-file/src/main/java/com/example/Main.java
+++ java-basics/demo-file/src/main/java/com/example/Main.java
@@ -4,11 +4,12 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-        Path path = Path.of("/home/ubuntu/hoge/fuga.txt");
+        Path path = Path.of("fuga.txt");
 
         System.out.println(path);
-        System.out.println(path.getRoot());
-        System.out.println(path.getParent());
-        System.out.println(path.getFileName());
+        System.out.println(path.toAbsolutePath());
+
+        Path path2 = Path.of("");
+        System.out.println(path2.toAbsolutePath());
     }
 }
