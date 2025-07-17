7e7d08a06daac16b496cba63a2bf245f63f6f11f
yusay1498 <u.yusay1498@gmail.com>
2025-07-17 11:40:14 +0900

    check StandardCopyOption.REPLACE_EXISTING

--- java-basics/demo-file/src/main/java/com/example/Main.java
+++ java-basics/demo-file/src/main/java/com/example/Main.java
@@ -2,11 +2,12 @@ package com.example;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class Main {
     public static void main(String[] args) throws Exception{
         Path source = Path.of("foo/bar/note.txt");        // コピー元
         Path target = Path.of("temp/note.txt");           // コピー先
-        Files.copy(source, target);                          //コピーする
+        Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
     }
 }
