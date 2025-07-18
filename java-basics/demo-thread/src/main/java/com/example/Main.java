d9ac92cd4e011ce41b6da80853c63899ecc18ecf
yusay1498 <u.yusay1498@gmail.com>
2025-07-18 15:52:37 +0900

    check Runnable method

--- java-basics/demo-thread/src/main/java/com/example/Main.java
+++ java-basics/demo-thread/src/main/java/com/example/Main.java
@@ -2,6 +2,11 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("---- start main ----");
+
+        Thread t1 = new Thread(new Runnable1());
+        t1.start();
+
+        System.out.println("---- end main ----");
     }
 }
---
+++ java-basics/demo-thread/src/main/java/com/example/Runnable1.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class Runnable1 implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Hello Thread1");
+    }
+}
