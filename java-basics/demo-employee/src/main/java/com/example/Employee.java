a0a7f1484d509fc7f4ca13a445f7ced22dbf4822
yusay1498 <u.yusay1498@gmail.com>
2025-07-22 13:36:27 +0900

    check record class

---
+++ java-basics/demo-employee/src/main/java/com/example/Employee.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public record Employee(
+        int id,
+        String name,
+        int age,
+        String department,
+        int fee
+) {
+}
--- java-basics/demo-employee/src/main/java/com/example/Main.java
+++ java-basics/demo-employee/src/main/java/com/example/Main.java
@@ -2,6 +2,14 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("record Class");
+        Employee alice = new Employee(
+                1,
+                "alice",
+                25,
+                "経理部",
+                250000
+        );
+        System.out.println("alice -> " + alice);
     }
 }
