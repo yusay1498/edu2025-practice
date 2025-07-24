10dacf5a8cb65922012980710b989278aa53ec98
yusay1498 <u.yusay1498@gmail.com>
2025-07-24 17:43:05 +0900

    add demo junit basic package

---
+++ demo-junit-basic/src/main/java/com/example/demo_junit_basic/DemoJunitBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_junit_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJunitBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoJunitBasicApplication.class, args);
+	}
+
+}
---
+++ demo-junit-basic/src/main/java/com/example/demo_junit_basic/Employee.java
@@ -0,0 +1,9 @@
+package com.example.demo_junit_basic;
+
+public record Employee(
+        int id,
+        String name,
+        int age,
+        String department
+) {
+}
