3efcf2fd4e746abce017b1e7f5ffa2f7ccbdbc02
yusay1498 <u.yusay1498@gmail.com>
2025-08-01 11:12:59 +0900

    add demo spring mvc module

---
+++ demo-spring-mvc/src/test/java/com/example/demo_spring_mvc/DemoSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
