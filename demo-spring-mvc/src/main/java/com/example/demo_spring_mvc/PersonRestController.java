a50625978ee4ec0733be1a9a521456b4e40e1623
yusay1498 <u.yusay1498@gmail.com>
2025-08-01 11:59:38 +0900

    check getById method

--- demo-spring-mvc/src/main/java/com/example/demo_spring_mvc/PersonRestController.java
+++ demo-spring-mvc/src/main/java/com/example/demo_spring_mvc/PersonRestController.java
@@ -1,16 +1,28 @@
 package com.example.demo_spring_mvc;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/persons")
 public class PersonRestController {
-    @GetMapping()
+    @GetMapping
     public Person get() {
         return new Person(
                 "0001",
+                "Alice",
+                25
+        );
+    }
+
+    @GetMapping("{id}")
+    public Person getById(
+            @PathVariable String id
+    ) {
+        return new Person(
+                id,
                 "Alice",
                 25
         );
