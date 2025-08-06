0d4773e1b14ecb1042d84457bbf2216fb06fd6d1
yusay1498 <u.yusay1498@gmail.com>
2025-08-06 14:52:39 +0900

    add query to findHighPoints method

--- demo-jdbc/src/main/java/com/example/demo_jdbc/CardRestController.java
+++ demo-jdbc/src/main/java/com/example/demo_jdbc/CardRestController.java
@@ -2,6 +2,7 @@ package com.example.demo_jdbc;
 
 import org.springframework.jdbc.core.JdbcOperations;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
--- demo-jdbc/src/main/java/com/example/demo_jdbc/JdbcCardRepository.java
+++ demo-jdbc/src/main/java/com/example/demo_jdbc/JdbcCardRepository.java
@@ -1,23 +1,46 @@
 package com.example.demo_jdbc;
 
+import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcOperations;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @Repository
 public class JdbcCardRepository {
-    private final JdbcOperations jdbcOperations;
+    private final NamedParameterJdbcOperations jdbcOperations;
 
-    public JdbcCardRepository(JdbcOperations jdbcOperations) {
+    public JdbcCardRepository(NamedParameterJdbcOperations jdbcOperations) {
         this.jdbcOperations = jdbcOperations;
     }
     
     public List<Card> findHighPoints() {
-        return List.of(
-                new Card(
-                        1, "card", 1, 1, 1
-                )
-        );
+        List<Card> cards = jdbcOperations.query("""
+                WITH
+                  card_sum AS (
+                    SELECT
+                      card.id, card.name, card.level, card.element_id, (card.top + card.right + card.bottom + card.left) AS summary
+                    FROM card
+                  ),
+                  card_sum_max AS (
+                    SELECT
+                      card.level, MAX(card.top + card.left + card.bottom + card.right) AS summary_max
+                    FROM card
+                    GROUP BY card.level
+                  )
+                
+                SELECT * FROM card_sum INNER JOIN card_sum_max
+                ON  card_sum.level = card_sum_max.level
+                AND card_sum.summary = card_sum_max.summary_max
+                
+                ORDER BY card_sum.level, card_sum.id;
+                """,
+                new MapSqlParameterSource(),
+                DataClassRowMapper.newInstance(Card.class));
+
+        return cards;
     }
 }
