9fdf3c9bb21d4599bc62737157011590a4dec531
yusay1498 <u.yusay1498@gmail.com>
2025-08-20 14:34:21 +0900

    add findById test

---
+++ demo-jdbc/src/test/java/com/example/demo_jdbc/JdbcCardRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.demo_jdbc;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+@DataJdbcTest
+@Import(TestContainerConfig.class)
+class JdbcCardRepositoryTest {
+    @DynamicPropertySource
+    static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.sql.init.mode", () -> "always");
+    }
+
+    @Autowired
+    JdbcClient jdbcClient;
+
+    @Sql(statements = {
+            """
+            INSERT INTO card(id, name, level, element_id, top, "right", bottom, "left") \
+            VALUES(1000, 'Test Card', 1 , 100, 1, 2, 3, 4)
+            """
+    })
+    @Test
+    void testFindById() {
+        JdbcCardRepository cardRepo = new JdbcCardRepository(jdbcClient);
+
+        Card card = cardRepo.findById(1000);
+
+        Assertions.assertThat(card).isNotNull();
+        Assertions.assertThat(card.id()).isEqualTo(1000);
+        Assertions.assertThat(card.name()).isEqualTo("Test Card");
+    }
+
+}
