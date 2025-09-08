70057892f6d25c09d0878394439a81b5eadfb1dc
yusay1498 <u.yusay1498@gmail.com>
2025-09-08 15:36:58 +0900

    create application service

---
+++ demo-attendance/src/main/java/com/example/demo_attendance/application/AttendanceService.java
@@ -0,0 +1,22 @@
+package com.example.demo_attendance.application;
+
+import com.example.demo_attendance.domain.entity.Attendance;
+import com.example.demo_attendance.domain.repository.AttendanceRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AttendanceService {
+    private final AttendanceRepository attendanceRepository;
+
+    public AttendanceService(AttendanceRepository attendanceRepository) {
+        this.attendanceRepository = attendanceRepository;
+    }
+
+    public List<Attendance> viewAllAttendances() {
+        return attendanceRepository.findAll();
+    }
+}
