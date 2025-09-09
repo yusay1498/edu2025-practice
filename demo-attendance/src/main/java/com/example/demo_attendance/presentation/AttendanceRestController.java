45421f62aa2c81f8ba04bc43e5eb260508538ab6
yusay1498 <u.yusay1498@gmail.com>
2025-09-09 16:01:35 +0900

    add post method to controller

--- demo-attendance/src/main/java/com/example/demo_attendance/presentation/AttendanceRestController.java
+++ demo-attendance/src/main/java/com/example/demo_attendance/presentation/AttendanceRestController.java
@@ -3,11 +3,10 @@ package com.example.demo_attendance.presentation;
 import com.example.demo_attendance.application.AttendanceService;
 import com.example.demo_attendance.domain.entity.Attendance;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -29,7 +28,30 @@ public class AttendanceRestController {
             @PathVariable String id
     ) {
         return attendanceService.viewAttendanceById(id)
+                // mapでOptionalが値を持っていたらResponseEntity::ok
+                // ResponseEntity::okは、Attendanceオブジェクトが存在する場合に、
+                // そのオブジェクトを含むHTTP 200 OKレスポンスを作成
                 .map(ResponseEntity::ok)
+                // ResponseEntity.notFound().build()は、HTTP 404 Not Foundレスポンスを作成
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<?> postAttendance(
+            @RequestBody Attendance attendance
+    ) {
+        // ResponseEntityにすることでHTTPステータスコードを制御
+        Attendance newAttendance = attendanceService.recordBeginWork(attendance.employeeId());
+
+        // 新しいリソースのURIを作成する
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newAttendance.id())
+                .toUri();
+
+        // Createdステータスと新しいリソースのURIを返す
+        // リソースが正常に作成されたことを示すために適切なHTTPステータスコードで応答
+        return ResponseEntity.created(location).build();
     }
 }
