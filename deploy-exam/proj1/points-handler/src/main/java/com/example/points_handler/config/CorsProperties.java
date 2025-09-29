32fe7d51b1f33489e7ea61f618d0a24b76cc9c24
yusay1498 <u.yusay1498@gmail.com>
2025-09-29 16:31:32 +0900

    add setting for front

---
+++ deploy-exam/proj1/points-handler/src/main/java/com/example/points_handler/config/CorsProperties.java
@@ -0,0 +1,19 @@
+package com.example.points_handler.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@ConfigurationProperties(prefix = "app.cors")
+public class CorsProperties {
+
+    private List<String> allowedOrigins;
+
+    public List<String> getAllowedOrigins() {
+        return allowedOrigins;
+    }
+
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+}
---
+++ deploy-exam/proj1/points-handler/src/main/java/com/example/points_handler/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.example.points_handler.config;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableConfigurationProperties(CorsProperties.class)
+public class WebConfig implements WebMvcConfigurer {
+    private final CorsProperties corsProperties;
+
+    public WebConfig(CorsProperties corsProperties) {
+        this.corsProperties = corsProperties;
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // /salesHistories以外のエンドポイントも含め、全てのエンドポイントにCORSを設定
+        registry.addMapping("/**")
+                .allowedOrigins(corsProperties.getAllowedOrigins().toArray(String[]::new))  // 許可するオリジン
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 許可するメソッド
+                .allowedHeaders("*")  // 任意のヘッダーを許可
+                .allowCredentials(true);  // クレデンシャル情報（例：クッキー）の送信を許可
+    }
+}
