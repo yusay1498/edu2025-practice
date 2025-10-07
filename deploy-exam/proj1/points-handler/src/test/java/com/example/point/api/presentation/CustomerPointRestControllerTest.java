ffb2e04fe5ffadcad9071b9ed6b7d47071b10815
yusay1498 <u.yusay1498@gmail.com>
2025-10-07 10:45:35 +0900

    update test by MockMvcTester

--- deploy-exam/proj1/points-handler/src/test/java/com/example/point/api/presentation/CustomerPointRestControllerTest.java
+++ deploy-exam/proj1/points-handler/src/test/java/com/example/point/api/presentation/CustomerPointRestControllerTest.java
@@ -1,95 +1,74 @@
 package com.example.point.api.presentation;
 
-import com.example.point.api.config.TestcontainersConfiguration;
-import org.hamcrest.Matchers;
+import com.example.point.api.application.CustomerNotFoundException;
+import com.example.point.api.application.CustomerPointService;
+import com.example.point.api.domain.entity.CustomerPoint;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+import org.springframework.test.web.servlet.assertj.MvcTestResult;
 
-import java.nio.charset.StandardCharsets;
-
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-@Import(TestcontainersConfiguration.class)
+@WebMvcTest(CustomerPointRestController.class)
 public class CustomerPointRestControllerTest {
 
-    @DynamicPropertySource
-    static void registerProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.sql.init.mode", () -> "always");
-    }
-
     @Autowired
-    private MockMvc mockMvc;
+    private MockMvcTester mockMvcTester;
+
+    @MockitoBean
+    private CustomerPointService mockedCustomerPointService;
 
     @Test
-    void testGetPoints() throws Throwable {
-        mockMvc
-                .perform(MockMvcRequestBuilders
-                        .get("/points")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding(StandardCharsets.UTF_8)
-                ).andDo(MockMvcResultHandlers
-                        .print()
-                ).andExpect(MockMvcResultMatchers
-                        .status()
-                        .isOk()
-                );
-    }
+    void testGetPoints() {
+        MvcTestResult actual = mockMvcTester
+                .get()
+                .uri("/points")
+                .exchange();
 
-    @ParameterizedTest
-    @Sql(statements = {
-            """
-            INSERT INTO customer_points (customer_id, current_points)
-            VALUES ('testId1', 100);
-            """
-    })
-    @CsvSource(delimiterString =  "|", textBlock = """
-            testId1 | testId1
-            """)
-    void testGetPointByCustomerId(
-            String customerId, String expectedCustomerId
-            ) throws Throwable {
-        mockMvc
-                .perform(MockMvcRequestBuilders
-                        .get("/points/{customerId}", customerId)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding(StandardCharsets.UTF_8)
-                ).andDo(MockMvcResultHandlers
-                        .print()
-                ).andExpect(MockMvcResultMatchers
-                        .status()
-                        .isOk()
-                ).andExpect(MockMvcResultMatchers
-                        .jsonPath("$.customerId", Matchers.is((expectedCustomerId)))
-                );
+        Assertions.assertThat(actual).hasStatusOk();
     }
 
     @Test
-    void testGetPointsByCustomerId_withNonexistentCustomerId() throws Throwable {
-        mockMvc
-                .perform(MockMvcRequestBuilders
-                        .get("/points/{customerId}", "nonexistent_customerId")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .characterEncoding(StandardCharsets.UTF_8)
-                ).andDo(MockMvcResultHandlers
-                        .print()
-                ).andExpect(MockMvcResultMatchers
-                        .status()
-                        .isNotFound());
+    void testGetPointByCustomerId() {
+        CustomerPoint testCustomerPoint = new CustomerPoint("testId1", 100);
+
+        Mockito.doReturn(testCustomerPoint).when(mockedCustomerPointService).lookupCustomerPointById("testId1");
+
+        MvcTestResult actual = mockMvcTester
+                .get()
+                .uri("/points/{customerId}", "testId1")
+                .exchange();
+
+        String correctResponse = """
+                                {
+                                  "customerId":testId1,
+                                  "currentPoints":100
+                                }
+                                """;
+
+        Assertions.assertThat(actual)
+                .debug()
+                .hasStatusOk()
+                .bodyJson()
+                .isStrictlyEqualTo(correctResponse);
+    }
+
+    @Test
+    void testGetPointsByCustomerId_withNonexistentCustomerId() {
+        Mockito.doThrow(new CustomerNotFoundException("nonTestId"))
+                .when(mockedCustomerPointService)
+                .lookupCustomerPointById("nonTestId");
+
+        MvcTestResult actual = mockMvcTester
+                .get()
+                .uri("/points/{customerId}", "nonTestId")
+                .exchange();
+
+        Assertions.assertThat(actual).hasStatus(HttpStatus.NOT_FOUND);
     }
 
 }
