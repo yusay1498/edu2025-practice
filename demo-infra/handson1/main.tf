f2baafcbe9cd3814347c5f7dc8f13afb2564c292
yusay1498 <u.yusay1498@gmail.com>
2025-10-27 13:11:33 +0900

    add handson1

---
+++ demo-infra/handson1/main.tf
@@ -0,0 +1,4 @@
+resource "local_file" "foo" {
+  filename = "foo.txt"
+  content  = "bar"
+}
---
+++ demo-infra/handson1/terraform.tf
@@ -0,0 +1,9 @@
+terraform {
+  required_version = ">= 0.15"
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.0"
+    }
+  }
+}
