b6fe4afb8f589a6d1308ed5f927da4f6a28b1bdc
yusay1498 <u.yusay1498@gmail.com>
2025-11-10 10:15:45 +0900

    add db

---
+++ deploy-exam/proj2/db/schema.sql
@@ -0,0 +1,49 @@
+DROP TABLE IF EXISTS sales_items;
+DROP TABLE IF EXISTS sales;
+DROP TABLE IF EXISTS sales_summary;
+
+-- 売上情報テーブルの作成
+CREATE TABLE sales
+(
+    id          VARCHAR(40) PRIMARY KEY, -- 売上情報ID
+    date_time   TIMESTAMP NOT NULL,      -- 購入日時
+    discount    INT       NOT NULL,      -- 割引
+    paid_point  INT       NOT NULL,      -- ポイント支払い
+    paid_cash   INT       NOT NULL,      -- 現金支払い
+    total       INT       NOT NULL,      -- 合計金額
+    given_point INT       NOT NULL       -- 付与ポイント
+);
+
+-- 商品情報テーブルの作成
+CREATE TABLE sales_items
+(
+    id         VARCHAR(40) PRIMARY KEY,
+    sales_id   VARCHAR(40),                      -- 売上情報ID (外部キー)
+    item_id    INT          NOT NULL,            -- 商品ID
+    unit_price INT          NOT NULL,            -- 単価
+    quantity   INT          NOT NULL,            -- 数量
+    subtotal   INT          NOT NULL,            -- 小計
+    FOREIGN KEY (sales_id) REFERENCES sales (id) -- 外部キー制約
+);
+
+-- 売上集計情報テーブルの作成
+CREATE TABLE sales_summary
+(
+    id             VARCHAR(40) PRIMARY KEY, -- 売上情報ID
+    item_id        INT,                     -- 商品ID
+    date           DATE NOT NULL,           -- 購入日時
+    total_amount   INT  NOT NULL,           -- 売上金額
+    total_quantity INT  NOT NULL            -- 売上点数
+);
+
+-- sales_summary テーブルに一意制約を追加
+ALTER TABLE sales_summary
+    ADD CONSTRAINT unique_item_date UNIQUE (item_id, date);
+
+-- インデックス作成
+CREATE INDEX idx_sales_date_time ON sales (date_time);
+CREATE INDEX idx_sales_items_sales_id ON sales_items (sales_id);
+CREATE INDEX idx_sales_items_item_id ON sales_items (item_id);
+CREATE INDEX idx_sales_summary_item_id ON sales_summary (item_id);
+CREATE INDEX idx_sales_summary_date ON sales_summary (date);
+CREATE INDEX idx_sales_summary_date_item_id ON sales_summary (date, item_id);
